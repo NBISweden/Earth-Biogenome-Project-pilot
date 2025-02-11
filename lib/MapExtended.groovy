64183b20d14318714a1560990bc9c4ce9a77be91
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2025-02-11 16:08:02 +0100

    Add commonPrefix function to String class

--- lib/MapExtended.groovy
+++ lib/MapExtended.groovy
@@ -10,3 +10,18 @@ Map.metaClass.deepMerge = { Map rhs ->
     def lhs = delegate
     lhs.collectEntries { k, v -> rhs[k] instanceof Map ? [ (k): v.deepMerge( rhs[k] ) ] : [ (k): rhs[k]?:v ] } + rhs.subMap(rhs.keySet()-lhs.keySet())
 }
+
+/* Custom method for String class called commonPrefix.
+
+Returns a string of the longest common prefix between two strings
+*/
+String.metaClass.commonPrefix = { String last ->
+    def first = delegate
+    def min_length = Math.min( first.length(), last.length() )
+    def prefix_length = 0
+    while ( prefix_length < min_length && first.charAt(prefix_length) == last.charAt(prefix_length) ){
+        prefix_length++
+    }
+    // return common prefix without trailing alphanumeric characters.
+    first.substring(0, prefix_length).replaceAll(/[^a-zA-Z0-9]+$/, '')
+}
