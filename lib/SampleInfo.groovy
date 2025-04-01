143a740ae35da924ac0d0b31c62015474cf77fde
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2025-04-01 14:57:57 +0200

    Separate SampleInfo to it's own file

---
+++ lib/SampleInfo.groovy
@@ -0,0 +1,20 @@
+record SampleInfo (
+    String sampleName,
+    Integer taxId,
+    Integer geneticCode,
+    Integer mitoCode,
+    String domain,
+    Integer genomeSize,
+    Integer ploidy,
+    Integer haploidNumber
+) {
+    SampleInfo { assert !sampleName.blank }
+
+    boolean hasComponents(List<String> components) {
+        return components.every { this[it] }
+    }
+
+    boolean isEukaryota() {
+        return domain == 'Eukaryota'
+    }
+}
